15dcbcfc12d4ee234cbae90b9db011bfbcbea580
Benta <bentaakitwi500@gmail.com>
2023-03-13 07:48:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(myArray, surprise)
+{
+    myArray = ["Guadalupe", "Ollie", "Aki"];
+    const newArray = [ ];
+    for(let a = 0; a < myArray.length; a++)
+    {
+        newArray.push(`Thank you, ${myArray[a]}, for the wonderful ${surprise} gift!`);
+    }
+    return newArray;
+}
+function countDown(number)
+{
+    let count = 0;
+    while( number >= count)
+    {
+        console.log(number);
+        number -=1;
+    }
+}
+countDown(10);
+writeCards();
